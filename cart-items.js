8d1560e9a1a20fd1ab4a755bd8dff7b1ba752295
carmgrif <carmgrif@umich.edu>
2021-05-03 17:53:26 -0400

    get cartItems array

--- cart-items.js
+++ cart-items.js
@@ -7,34 +7,34 @@ const cartItems = express.Router();
 cartItems.use(express.json());
 
 cartItems.get("/cart-items", (req, res) => {
-    res.json("Getting all  students..");
+    res.json(cartItems);
 });
 
 
 cartItems.get("/cart-items/:id", (req, res) => {
-    res.json("Getting all students..");
+    res.json("Getting cart items by ID");
 });
 
 
 cartItems.get("/cart-items/:id", (req, res) => {
     console.log(req.params.id);
-    res.json("Getting a student.");
+    res.json("Getting cart items by ID");
 });
 
 
 cartItems.post("/cart-items", (req, res) => {
     console.log(req.body)
-    res.json("Adding a student..");
+    res.json("Adding cart items");
 });
 
 
 cartItems.put("/cart-items/:id", (req, res) => {
-    res.json("Updating a student..");
+    res.json("Updating cart items");
 });
 
 
 cartItems.delete("/cart-items/:id", (req, res) => {
-    res.json("Deleting a student..");
+    res.json("Deleting cart");
 });
 
 cartItems.get('/search', (req, res) => {
