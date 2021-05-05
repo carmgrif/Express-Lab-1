5d736ec332d6f4b03520373354f58d329083becb
carmgrif <carmgrif@umich.edu>
2021-05-05 18:27:38 -0400

    added functions to cart items file

--- cart-items.js
+++ cart-items.js
@@ -1,45 +1,113 @@
 
+const express = require("express");
+const router = express.Router();
+// const cartItems = require('./cart-items');
 
-const express = require('express');
-const cartItems = express.Router();
+// const app = express();
 
 
-cartItems.use(express.json());
+router.use(express.json());
 
-cartItems.get("/cart-items", (req, res) => {
+
+const cartItems = [
+    {
+        id: '48374',
+        product: 'mandarin oranges',
+        price: 3.99,
+        quantity: 1
+    },
+
+     {
+        id: '60385',
+        product: 'nectarines',
+        price: 5.99,
+        quantity: 8
+    },
+     
+      {
+        id: '91847',
+        product: 'spinach',
+        price: 0.89,
+        quantity: 1
+    },
+      
+    {
+        id: '73497',
+        product: 'bananas',
+        price: 2.50,
+        quantity: 3
+    }
+]
+
+
+
+// const port = 3000;
+
+// app.listen(3000, () => console.log('Listening on port 3000'));
+
+
+
+
+router.get("/cart-items", (req, res) => {
     res.json(cartItems);
 });
 
 
-cartItems.get("/cart-items/:id", (req, res) => {
-    res.json("Getting cart items by ID");
-});
-
-
-cartItems.get("/cart-items/:id", (req, res) => {
+router.get("/cart-items/:id", (req, res) => {
     console.log(req.params.id);
-    res.json("Getting cart items by ID");
+    const found = cartItems.find(item => item.id === parseInt(req.params.id));
+
+    if (!found) {
+        res.status(404).send('The cart item could not be found');
+    } else {
+        res.json(found);
+    }
 });
 
 
-cartItems.post("/cart-items", (req, res) => {
-    console.log(req.body)
-    res.json("Adding cart items");
+router.post("/cart-items", (req, res) => {
+    const quantity = parseInt(req.body.quantity);
+
+    if (!quantity) {
+        return res.status(400).send('Invalid quantity');
+    }
+
+    const price = parseInt(req.body.quantity);
+
+    if (!price) {
+        return res.status(400).send('Invalid price');
+    }
+
+    const newItem = {
+        id: cartItems.length + 1,
+        product: req.body.product,
+        price: req.body.price,
+        quantity: quantity
+    };
+
+    cartItems.push(newItem);
+    res.status(201).json(newItem);
+
 });
 
 
-cartItems.put("/cart-items/:id", (req, res) => {
-    res.json("Updating cart items");
+router.put("/cart-items/:id", (req, res) => {
+    
+    const found = cartItems.find(item => item.id === parseInt(req.params.id));
+
+    if (found) {
+        found.price = req.body.price;
+    }
 });
 
 
-cartItems.delete("/cart-items/:id", (req, res) => {
+router.delete("/cart-items/:id", (req, res) => {
     res.json("Deleting cart");
 });
 
-cartItems.get('/search', (req, res) => {
+router.get('/search', (req, res) => {
     console.log(req.query.keyword);
     res.json('Search results');
 })
 
-module.exports = cartItems;
+module.exports = router;
--- server.js
+++ server.js
@@ -3,35 +3,7 @@ const cartItems = require('./cart-items');
 
 const app = express();
 
-const cartItems = [
-    {
-        id: '48374',
-        product: 'mandarin oranges',
-        price: 3.99,
-        quantity: 1
-    },
 
-     {
-        id: '60385',
-        product: 'nectarines',
-        price: 5.99,
-        quantity: 8
-    },
-     
-      {
-        id: '91847',
-        product: 'spinach',
-        price: 0.89,
-        quantity: 1
-    },
-      
-    {
-        id: '73497',
-        product: 'bananas',
-        price: 2.50,
-        quantity: 3
-    }
-]
 
 app.use('/', cartItems);
 
